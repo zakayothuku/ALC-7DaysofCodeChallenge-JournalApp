6224e78ae17e9d4f9c29a73529d97d3ac005d815
Schatz Designs <zakayothuku@gmail.com>
2018-07-02 02:13:36 +0300

    Logout Menu

--- app/src/main/java/alc/sevendayschallenge/journal/activities/MainActivity.java
+++ app/src/main/java/alc/sevendayschallenge/journal/activities/MainActivity.java
@@ -103,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
